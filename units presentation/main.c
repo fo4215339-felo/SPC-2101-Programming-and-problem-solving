d6b518dec71829be4bb81b2f4a1a2257bf8d2fcf
fo4215339-felo <fo4215339@gmail.com>
2025-09-19 13:34:58 +0300

    Programme to display units

--- units presentation/main.c
+++ units presentation/main.c
@@ -1,15 +1,26 @@
+/*
+ 1. Name:FELIX OMONDI
+ 2.Reg No:PA106/G/28817/25
+ 3.Description :Programme to display all units.
+ */
+
+//main function
+
 #include <stdio.h>
 
 
 int main()
 {
-  printf("Introduction to Programming and Problem Solving\n");
-  printf("Communication Skills\n");
-  printf("Fundamentals of ICT\n");
-  printf("Mathematics for Science\n");
-  printf("Discrete Mathematics\n");
-  printf("Computer Systems and Organization\n");
-  printf("System Analysis and Design\n");
+ 
+  
+  printf("1.Introduction to Programming and Problem Solving\n");
+  printf("2.Communication Skills\n");
+  printf("3.Fundamentals of ICT\n");
+  printf("4.Mathematics for Science\n");
+  printf("5.Discrete Mathematics\n");
+  printf("6.Computer Systems and Organization\n");
+  printf("7.System Analysis and Design\n");
 
     return 0;
 }
+
