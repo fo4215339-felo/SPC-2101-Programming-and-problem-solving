426bfa7c48c1e9df2bdddc2eba94633360400c64
fo4215339-felo <fo4215339@gmail.com>
2025-10-06 22:04:36 +0300

    Add files via upload

---
+++ for_loop1.c
@@ -0,0 +1,23 @@
+*
+Name : Felix Omondi
+Reg NO : PA106/G/28817/25
+Decription : Program to display all numbers between 50 and 100 in descending order
+Date : 4 oct 2025
+*/
+
+//header file
+#include <stdio.h>
+
+//main function
+int main(){
+	
+	//for loop = repaets a section of a code a limited amount of times
+	//loop from 100 downwards to 50
+	for (int i =100; i >= 50; i--)
+	{
+		
+		printf("%d\n",i);
+		}
+	
+return 0;			
+}
